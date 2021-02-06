0e56be66423d66317a8b5826147ee6a92074e269
Joao C Barbosa <joaao@outlook.com>
2021-02-06 00:38:36 -0300

    feat: use special route to get all unhandled resources with an 404 status code

--- src/server.js
+++ src/server.js
@@ -4,13 +4,22 @@ const express = require('express');
 const app = express();
 const logger = require('./middleware/logger');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_STRING_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(cors());
 app.use(logger.logger);
 app.use(logger.loggerDev);
 app.use(express.json());
 app.use(require('./routes'));
+app.use('*', (req, res) => {
+    res.status(404).send({ message: 'Resource not found' });
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`May the force be with you on port ${process.env.APP_PORT}`);
 });
