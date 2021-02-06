78518a9147f2e685b3da7b60350ce19ad9132950
Joao C Barbosa <joaao@outlook.com>
2021-02-06 12:03:03 -0300

    feat: create DELETE mock resource

--- src/controllers/mockController.js
+++ src/controllers/mockController.js
@@ -23,8 +23,15 @@ const createMock = async (req, res) => {
 const getMock = async (req, res) => {
     const mockId = req.params.mockId;
 
+    console.log(mockId);
+
     try {
         const mock = await Mock.findById(mockId);
+
+        return res.json({
+            mock: mock,
+            url: `${process.env.APP_HOST}/${mock._id}`
+        });
     } catch (err) {
         res.status(500).json({
             message: err
@@ -32,7 +39,28 @@ const getMock = async (req, res) => {
     }
 }
 
+
+const deleteMock = async (req, res) => {
+    const mockId = req.params.mockId;
+
+    const checkMock = await Mock.findById(mockId);
+
+    if (!checkMock) {
+        return res.status(404).json({ message: 'Mock not found' });
+    }
+
+    try {
+        await Mock.findByIdAndDelete(mockId);
+
+        return res.send();
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+}
+
+
 module.exports = {
     getMock,
-    createMock
+    createMock,
+    deleteMock
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const mockController = require('./controllers/mockController');
 
 routes.post('/mock', mockController.createMock)
 routes.get('/mock/:mockId', mockController.getMock);
+routes.delete('/mock/:mockId', mockController.deleteMock);
 
 module.exports = routes;
